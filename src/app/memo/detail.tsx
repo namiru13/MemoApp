f7efc7d95905cf6ac677d966d45b9698fb472af2
namiru <149836901+namiru13@users.noreply.github.com>
2024-10-01 22:06:02 +0900

    Create Icon EditView

--- src/app/memo/detail.tsx
+++ src/app/memo/detail.tsx
@@ -2,6 +2,7 @@ import { View, Text, ScrollView, StyleSheet} from 'react-native'
 
 import Header from '../../components/Header'
 import CircleButton from '../../components/CircleButton'
+import Icon from '../../components/Icon'
 
 const Detail = () =>{
     return(
@@ -15,10 +16,12 @@ const Detail = () =>{
                 <Text style={style.memoBodyText}>
                     買い物リスト
                     書体やレイアウトなどを確認するために用います。
-                    本文用なので使い方を間違えると不自然に見えることもありますので要注意。
+                    本文用なので使い方を間違えると不自然に見えることもありますので要注意
                 </Text>
             </ScrollView>
-            <CircleButton style={{ top:160, bottom:'auto'}}>＋</CircleButton>
+            <CircleButton style={{ top:160, bottom:'auto'}}>
+                <Icon name = 'pencil' size={40} color = '#ffffff' />
+            </CircleButton>
         </View>
 
     )
