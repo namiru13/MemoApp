2a9ce17d7c94a1bc1418c250205ef36d85f87236
namiru <149836901+namiru13@users.noreply.github.com>
2024-10-20 21:28:22 +0900

    Get MemoList

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -1,42 +1,48 @@
 import { 
-    View, TextInput, StyleSheet, KeyboardAvoidingView
- } from 'react-native'
+    View, TextInput, StyleSheet } from 'react-native'
 import { router } from 'expo-router'
-import { collection, addDoc } from 'firebase/firestore'
+import { collection, addDoc, Timestamp } from 'firebase/firestore'
+import { useState } from 'react'
 
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/Icon'
-import { db } from '../../config'
+import { db, auth } from '../../config'
+import KeyboardAvoidingView from '../../components/KeyboardAvoidingView'
 
-const handlePress = async () =>{
-    // addDoc( collection(db, 'memos') , {
-    //     bodyText: 'test'
-    // })
-    // .then((docRef) => {
-    //     console.log('success', docRef.id)
-    //     router.back()
-    // })
-    // .catch((error) =>{
-    //     console.log(error)
-    // })
+const handlePress = (bodyText: string):void =>{
+    if (auth.currentUser == null) {return}
+    const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
 
-    await addDoc(collection(db, 'memos'),{
-        bodyText: 'test 2'
+    addDoc( ref , {
+        bodyText,
+        updatedAt: Timestamp.fromDate( new Date())
     })
-    .catch((error) => {
+    .then((docRef) => {
+        console.log('success', docRef.id)
+        router.back()
+    })
+    .catch((error) =>{
         console.log(error)
     })
-    router.back()
+
+
 }
 
 const Create = ():JSX.Element => {
+    const [bodyText, setBodyText] = useState('')
     return(
-        <KeyboardAvoidingView behavior='height' style ={styles.container}>
+        <KeyboardAvoidingView style ={styles.container}>
             <View style ={styles.inputContainer}>
-                <TextInput multiline style = {styles.input} value = '' />
+                <TextInput 
+                multiline 
+                style = {styles.input} 
+                value = {bodyText} 
+                onChangeText ={ (text) => {setBodyText(text)}}
+                autoFocus
+                />
             </View>
 
-            <CircleButton onPress={handlePress}>
+            <CircleButton onPress={()=> {handlePress(bodyText)}}>
                 <Icon name = 'check' size = {40} color='#ffffff' />
             </CircleButton>
         </KeyboardAvoidingView>
